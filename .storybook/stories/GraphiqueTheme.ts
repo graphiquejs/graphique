6a6fd93cc2c903918e8ba8649d4418d90d26511c
Matt Adams <matt-adams@pluralsight.com>
2021-01-11 18:02:25 -0500

    include logo img for storybook

--- .storybook/stories/GraphiqueTheme.ts
+++ .storybook/stories/GraphiqueTheme.ts
@@ -3,7 +3,5 @@ import { create } from "@storybook/theming"
 export default create({
   base: "light",
   brandTitle: "Graphique",
-  // brandUrl: "https://graphique.mattadams.io",
-  brandImage:
-    "https://mattadams.io/static/media/graphique_logo_serif_trans.7ccc6d1e.png",
+  brandImage: require("./assets/graphique_logo_serif_trans.png")
 })
