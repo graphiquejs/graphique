dc36e73146f9930899a3532eb98263d7d56bc4aa
Matt Adams <matt-adams@pluralsight.com>
2021-01-12 10:34:00 -0500

    use cloudinary for img available via url

--- .storybook/stories/GraphiqueTheme.ts
+++ .storybook/stories/GraphiqueTheme.ts
@@ -3,5 +3,5 @@ import { create } from "@storybook/theming"
 export default create({
   base: "light",
   brandTitle: "Graphique",
-  brandImage: require("./assets/graphique_logo_serif_trans.png")
+  brandImage: "https://res.cloudinary.com/mattadams/image/upload/v1610465342/graphique_logo_serif_trans_nvmxw0.png"
 })
