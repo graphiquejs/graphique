4950adeac0849f68b9242e50b1d3ae5e82bd3649
Matt Adams <madams@seatgeek.com>
2022-03-06 09:45:04 -0500

    account for date grouping

--- packages/graphique/src/util/widen.ts
+++ packages/graphique/src/util/widen.ts
@@ -6,7 +6,7 @@ export const widen = (
   getGroup: (d: unknown) => any,
   count: (d: unknown) => any
 ) => {
-  const pivots = Array.from(new Set(data.map(pivot)))
+  const pivots = Array.from(new Set(data.map(d => isDate(pivot(d)) ? pivot(d).valueOf() : pivot(d))))
   const groups = Array.from(new Set(data.map(getGroup)))
   return pivots.map((p, i) => {
     const out: any = { key: isDate(p) ? p.valueOf() : p, i }
@@ -14,8 +14,8 @@ export const widen = (
       const pivotGroup = data.find(
         d =>
           (isDate(pivot(d))
-            ? pivot(d).valueOf() === p.valueOf()
-            : pivot(d) === p) && getGroup(d) === g
+            ? pivot(d).valueOf() === p.valueOf() && getGroup(d) === g
+            : pivot(d) === p && getGroup(d) === g)
       )
       if (pivotGroup) {
         out[g] = count(pivotGroup) ?? undefined
