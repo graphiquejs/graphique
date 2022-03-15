51d3ba73e93baaa145f2b724a10a56cb2e3c6391
madams1 <matt@mattadams.io>
2022-03-15 16:46:46 -0400

    better group accessor definitions

--- packages/graphique/src/util/defineGroupAccessor.ts
+++ packages/graphique/src/util/defineGroupAccessor.ts
@@ -1,12 +1,12 @@
 import { Aes } from "../gg";
 
 export const defineGroupAccessor = (aes: Aes, allowUndefined = false) => {
-    if (!aes || allowUndefined) return undefined
+    if (!aes && allowUndefined) return undefined
     return (
-        aes.group ||
-        aes.fill ||
-        aes.stroke ||
-        aes.strokeDasharray ||
+        aes?.group ||
+        aes?.fill ||
+        aes?.stroke ||
+        aes?.strokeDasharray ||
         (allowUndefined ? undefined : () => '__group')
     )
 }
