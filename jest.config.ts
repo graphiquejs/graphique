4c0cc9de669b9bdf459d9ab6aa226babcfd243ac
madams1 <matt@mattadams.io>
2023-07-21 11:57:05 -0400

    testing tooling + config

---
+++ jest.config.ts
@@ -0,0 +1,32 @@
+import type { Config } from 'jest'
+
+const config: Config = {
+  preset: 'ts-jest',
+  resetModules: true,
+  transform: {
+		'^.+\\.(ts|tsx)?$': 'ts-jest',
+	},
+  testEnvironment: 'jest-environment-jsdom',
+  coveragePathIgnorePatterns: [
+    "jest.config.js",
+    "/node_modules/",
+    "/dist/",
+  ],
+  moduleNameMapper: {
+    '^d3-((?!(delaunay|interpolate-path)).*)': '<rootDir>/node_modules/d3-$1/dist/d3-$1',
+    '^d3-((delaunay).*)': '<rootDir>/packages/graphique/node_modules/d3-$1/dist/d3-$1.min.js',
+    '^nanoid': '<rootDir>/node_modules/nanoid/index.cjs',
+    '^@graphique/(.*)$': '<rootDir>/packages/$1/'
+  },
+  setupFilesAfterEnv: [
+    '<rootDir>/jest.setup.ts',
+    '@testing-library/jest-dom/extend-expect',
+  ],
+  roots: ['<rootDir>/packages/'],
+  testPathIgnorePatterns: [
+    '/__tests__/__data__',
+    '/__tests__/shared'
+  ],
+}
+
+export default config
