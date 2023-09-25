9f13d9434186aedd8a4932e16b0361eeacef785b
madams1 <matt@mattadams.io>
2023-09-25 12:05:25 -0400

    hline testing

---
+++ packages/geom-hline/src/__tests__/GeomHLineBasics.test.tsx
@@ -0,0 +1,80 @@
+import React from 'react'
+import { render, screen, act } from '@testing-library/react'
+import { penguins } from '@graphique/datasets'
+import {
+  GGHLine,
+  NUM_GROUPS,
+  DEFAULT_GROUP_STROKES,
+  DEFAULT_AES,
+  DEFAULT_STROKE_WIDTH,
+} from "./shared"
+import { flipperLengthsBySpecies, type PenguinSummary } from './shared'
+import { GeomHLine } from '../geomHLine'
+
+jest.useFakeTimers()
+
+describe('horizontal line basics with GeomHLine', () => {
+  it('renders line for each group with stroke aes mapping', async () => {
+    render(
+      <GGHLine
+        aes={{
+          ...DEFAULT_AES,
+          y: (d: PenguinSummary) => d.count!
+        }}
+      />
+    )
+
+    act(() => jest.runAllTimers())
+
+    // exists
+    const chart = screen.queryByTestId('__gg_gg')
+    expect(chart).toBeInTheDocument()
+
+    // lines are formatted as expected
+    const hLines = await screen.findAllByTestId('__gg_geom_hline')
+    expect(hLines).toHaveLength(NUM_GROUPS)
+
+    hLines.forEach((v, i) => {
+      expect(v).toHaveAttribute('stroke', DEFAULT_GROUP_STROKES[i])
+      expect(v).toHaveAttribute('stroke-width', DEFAULT_STROKE_WIDTH)
+    })
+
+    // no x axis/ticks because no x aes mapping to data
+    const xTick = await screen.findByTestId('__gg_x_tick_label')
+    expect(xTick.textContent).toBeFalsy()
+  })
+
+  it('renders line for each group with local aesthetics and data', async () => {
+    render(
+      <GGHLine data={penguins}>
+        <GeomHLine
+          data={flipperLengthsBySpecies}
+          aes={{
+            y: (d: PenguinSummary) => d.count!
+          }}
+        />
+      </GGHLine>
+    )
+
+    act(() => jest.runAllTimers())
+
+     // exists
+    const chart = screen.queryByTestId('__gg_gg')
+    expect(chart).toBeInTheDocument()
+
+    // lines are formatted as expected
+    const hLines = await screen.findAllByTestId('__gg_geom_hline')
+    expect(hLines).toHaveLength(NUM_GROUPS)
+
+    hLines.forEach((v, i) => {
+      expect(v).toHaveAttribute('stroke', DEFAULT_GROUP_STROKES[i])
+      expect(v).toHaveAttribute('stroke-width', DEFAULT_STROKE_WIDTH)
+    })
+
+    // has numeric x axis/ticks from x aes mapping to data
+    const xTicks = await screen.findAllByTestId('__gg_x_tick_label')
+    xTicks.forEach((tick) => {
+      expect(Number(tick.textContent)).toBeGreaterThan(30)
+    })
+  })
+})
---
+++ packages/geom-hline/src/__tests__/GeomHLineSnapshot.test.tsx
@@ -0,0 +1,70 @@
+import React from 'react'
+import { Penguin, penguins } from '@graphique/datasets'
+import { act, screen } from '@testing-library/react'
+import {
+  GGHLine,
+  DEFAULT_AES,
+  flipperLengthsBySpecies,
+  setup,
+  type PenguinSummary,
+} from './shared'
+import { GeomHLine } from '../geomHLine'
+import { GeomPoint } from '../../../geom-point/dist'
+
+// useful for controlling the randomly-generated ID given to graphique objects
+jest.mock('nanoid', () => ({
+  nanoid: jest.fn(() => 'test-id')
+}))
+
+jest.useFakeTimers()
+
+describe('a chart with vertical lines matches a snapshot', () => {
+  it('with multiple settings', async () => {
+    const { asFragment } = setup(
+      <GGHLine data={penguins}>
+        <GeomHLine
+          data={flipperLengthsBySpecies}
+          aes={{
+            y: (d: PenguinSummary) => d.count!,
+          }}
+          strokeDasharray='2,1'
+        />
+      </GGHLine>
+    )
+
+    act(() => jest.runAllTimers())
+
+    expect(asFragment()).toMatchSnapshot()
+  })
+
+  it('with another geom', async () => {
+    const { user, asFragment } = setup(
+      <GGHLine
+        data={penguins.filter(d => DEFAULT_AES.x(d) && DEFAULT_AES?.y?.(d))}
+        aes={{
+          ...DEFAULT_AES,
+          fill: (d: Penguin) => d.species
+        }}
+      >
+        <GeomHLine
+          data={flipperLengthsBySpecies}
+          aes={{
+            y: (d: PenguinSummary) => d.count!,
+          }}
+          strokeDasharray='2,1'
+          showTooltip={false}
+        />
+        <GeomPoint />
+      </GGHLine>
+    )
+
+    act(() => jest.runAllTimers())
+
+    const eventArea = await screen.findByTestId('__gg_event_area')
+    await user.pointer({ target: eventArea, coords: { clientX: 250, clientY: 200 } })
+
+    act(() => jest.runOnlyPendingTimers())
+    
+    expect(asFragment()).toMatchSnapshot()
+  })
+})
