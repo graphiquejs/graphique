d2f765edbcd73032da7fbb81fcd1c78aa277e3b5
madams1 <matt@mattadams.io>
2023-10-02 11:16:18 -0400

    GeomArea tests

---
+++ packages/geom-area/src/__tests__/GeomAreaBasics.test.tsx
@@ -0,0 +1,163 @@
+/* eslint-disable react/jsx-props-no-spreading */
+import React from 'react'
+import { render, screen, act } from '@testing-library/react'
+import { type Stock } from '@graphique/datasets'
+import {
+  DEFAULT_AREA_PROPS,
+  DEFAULT_FILL,
+  DEFAULT_GROUP_FILLS,
+  GGArea,
+  NUM_GROUPS,
+  DEFAULT_AES,
+  undefinedYValData,
+} from "./shared"
+import { GeomArea } from '../geomArea'
+
+jest.useFakeTimers()
+
+const runBasicAreaChecks = async () => {
+  act(() => jest.runAllTimers())
+
+  // exists
+  const chart = screen.queryByTestId('__gg_gg')
+  expect(chart).toBeInTheDocument()
+
+  // area ranges are there
+  const areas = await screen.findAllByTestId('__gg_geom_area')
+  expect(areas).toHaveLength(NUM_GROUPS)
+  
+  // with default fill colors
+  const areaFills = areas.map(a => a.getAttribute('fill'))
+  const hasSameNumFills = areaFills.length === DEFAULT_GROUP_FILLS.length
+  expect(
+    hasSameNumFills &&
+      areaFills.every((fill, i) => fill === DEFAULT_GROUP_FILLS[i])
+  ).toBeTruthy()
+}
+
+describe('area chart basics with GeomArea', () => {
+  it('renders single area ranges for x,y aesthetic mappings', async () => {
+    render(
+      <GGArea
+        {...DEFAULT_AREA_PROPS}
+        data={DEFAULT_AREA_PROPS.data.filter(d => d.symbol === 'AAPL')}
+        aes={{
+          ...DEFAULT_AREA_PROPS.aes,
+          fill: undefined
+        }}
+      />
+    )
+
+    act(() => jest.runAllTimers())
+
+    // exists
+    const chart = screen.queryByTestId('__gg_gg')
+    expect(chart).toBeInTheDocument()
+
+    // singe area range is there
+    const area = await screen.findByTestId('__gg_geom_area')
+    expect(area).toBeInTheDocument()
+    expect(area).toHaveAttribute('fill', DEFAULT_FILL)
+  })
+  
+  it('renders multiple area ranges for x,y,fill aesthetic mappings', async () => {
+    render(<GGArea {...DEFAULT_AREA_PROPS} />)
+
+    runBasicAreaChecks()
+  })
+
+  it('renders multiple area ranges with y0,y1', async () => {
+    render(
+      <GGArea
+        {...DEFAULT_AREA_PROPS}
+        aes={{
+          ...DEFAULT_AES,
+          y: undefined,
+        }}
+      >
+        <GeomArea
+          aes={{
+            y0: (d: Stock) => d.marketCap - (d.marketCap * 0.1),
+            y1: (d: Stock) => d.marketCap + (d.marketCap * 0.1) 
+          }}
+        />
+      </GGArea>
+    )
+
+    runBasicAreaChecks()
+  })
+
+  it('renders stacked areas as expected', async () => {
+    render(
+      <GGArea {...DEFAULT_AREA_PROPS}>
+        <GeomArea position='stack' />
+      </GGArea>
+    )
+
+    runBasicAreaChecks()
+  })
+
+  it('renders filled areas as expected', async () => {
+    render(
+      <GGArea {...DEFAULT_AREA_PROPS}>
+        <GeomArea position='fill' />
+      </GGArea>
+    )
+
+    runBasicAreaChecks()
+  })
+
+  it('renders discontinuous area paths when given missing y values', async () => {
+    render(<GGArea {...DEFAULT_AREA_PROPS} data={undefinedYValData} />)
+
+    runBasicAreaChecks()
+  })
+
+  it('has no areas when empty data', () => {
+    render(<GGArea {...DEFAULT_AREA_PROPS} data={[]} />)
+
+    const chart = screen.queryByTestId('__gg_gg')
+    expect(chart).toBeInTheDocument()
+
+    const areas = screen.queryAllByTestId('__gg_geom_area')
+    expect(areas).toHaveLength(0)
+  })
+
+  it('throws error with misspecified y1 aes', () => {
+    expect(() => {
+      render(
+        <GGArea {...DEFAULT_AREA_PROPS}>
+          <GeomArea
+            aes={{
+              y1: (d: Stock) => d.marketCap + (d.marketCap * 0.1)
+            }}
+          />
+        </GGArea>
+      )
+    })
+      .toThrowError('GeomArea: aes.y1 can only be specified when combined with aes.y0')
+  })
+
+  it('throws error with position="stack" + missing y aes', () => {
+    expect(() => {
+      render(
+        <GGArea
+          {...DEFAULT_AREA_PROPS}
+          aes={{
+            ...DEFAULT_AES,
+            y: undefined
+          }}
+        >
+          <GeomArea
+            aes={{
+              y0: (d: Stock) => d.marketCap - (d.marketCap * 0.1),
+              y1: (d: Stock) => d.marketCap + (d.marketCap * 0.1)
+            }}
+            position='stack'
+          />
+        </GGArea>
+      )
+    })
+      .toThrowError('GeomArea: aes.y is required when using position="stack"')
+  })
+})
---
+++ packages/geom-area/src/__tests__/GeomAreaSnapshot.test.tsx
@@ -0,0 +1,133 @@
+/* eslint-disable react/jsx-props-no-spreading */
+import React from 'react'
+import { type Stock } from '@graphique/datasets'
+import {
+  GG,
+  Theme,
+  type DataValue,
+  type AreaPositions,
+  type Aes,
+} from '@graphique/graphique'
+import { act, screen } from '@testing-library/react'
+import { type UserEvent } from '@testing-library/user-event'
+import { DEFAULT_AES, DEFAULT_DATA, setup, undefinedYValData } from './shared'
+import { GeomArea, Legend } from '..'
+
+// useful for controlling the randomly-generated ID given to graphique objects
+jest.mock('nanoid', () => ({
+  nanoid: jest.fn(() => 'test-id')
+}))
+
+jest.useFakeTimers()
+
+interface TestComponentProps {
+  aes?: Aes
+  data?: Stock[]
+  position?: AreaPositions
+  y0?: DataValue
+  y1?: DataValue
+}
+
+const TestComponent = ({
+  aes = DEFAULT_AES,
+  data = DEFAULT_DATA,
+  position = 'identity',
+  y0,
+  y1,
+}: TestComponentProps) => (
+  <GG
+    data={data}
+    aes={aes}
+    margin={{ left: 50 }}
+  >
+    <GeomArea
+      position={position}
+      opacity={0.5}
+      aes={{ y0, y1 }}
+    />
+    <Theme animationDuration={0} />
+    <Legend title='Stocks' orientation='horizontal' />
+  </GG>
+)
+
+const prepareTesting = async (user: UserEvent) => {
+  act(() => jest.runAllTimers())
+
+  await screen.findAllByTestId('__gg_geom_area')
+
+  const eventArea = await screen.findByTestId('__gg_event_area')
+  await user.pointer({ target: eventArea, coords: { clientX: 200, clientY: 200 } })
+  await screen.findByTestId('__gg_x_tooltip')
+  
+  act(() => jest.runOnlyPendingTimers())
+}
+
+describe('area charts match snapshots', () => {
+  it('simple, single area', async () => {
+    const { user, asFragment } = setup(
+      <TestComponent
+        data={DEFAULT_DATA.filter(d => d.symbol === 'AAPL')}
+        aes={{
+          ...DEFAULT_AES,
+          fill: undefined
+        }}
+      />
+    )
+
+    await prepareTesting(user)
+    expect(asFragment()).toMatchSnapshot()
+  })
+
+  it('position="identity" (default), filled', async () => {
+    const { user, asFragment } = setup(<TestComponent />)
+    await prepareTesting(user)
+
+    expect(asFragment()).toMatchSnapshot()
+  })
+
+  it('position="stack", filled', async () => {
+    const { user, asFragment } = setup(
+      <TestComponent position='stack' />
+    )
+    await prepareTesting(user)
+
+    expect(asFragment()).toMatchSnapshot()
+  })
+
+  it('position="fill", filled', async () => {
+    const { user, asFragment } = setup(
+      <TestComponent position='fill' />
+    )
+    await prepareTesting(user)
+    
+    expect(asFragment()).toMatchSnapshot()
+  })
+
+  it('y0, y1 set — grouped "ribbons"', async () => {
+    const { user, asFragment } = setup(
+      <TestComponent
+        aes={{
+          ...DEFAULT_AES,
+          y: undefined,
+        }}
+        y0={(d: Stock) => d.marketCap - (d.marketCap * 0.1)}
+        y1={(d: Stock) => d.marketCap + (d.marketCap * 0.1)}
+      />
+    )
+    await prepareTesting(user)
+    
+    expect(asFragment()).toMatchSnapshot()
+  })
+
+  it('discontinuous data, filled', async () => {
+    const { user, asFragment } = setup(
+      <TestComponent
+        position='stack'
+        data={undefinedYValData}
+      />
+    )
+    await prepareTesting(user)
+    
+    expect(asFragment()).toMatchSnapshot()
+  })
+})
